c52c1e730534a2f00c55947dad1033b5b1386501
kali <kali@kali.kali>
2026-05-11 20:44:57 -0300

    new scan scripts

---
+++ dns_recon.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export PATH=$PATH:$HOME/go/bin
+
+BUG_BOUNTY_DIR="/home/kali/bugbounty"
+
+# Verifica se a flag --brute foi passada
+ENABLE_BRUTE=false
+
+if [[ "$1" == "--brute" ]]; then
+    ENABLE_BRUTE=true
+fi
+
+find "$BUG_BOUNTY_DIR" -type f -name "domains.txt" | while read -r domain_path; do
+
+    COMPANY_DIR=$(dirname "$domain_path")
+
+    parallel -j 4 --bar "
+        DOMAIN={}
+
+        DOMAIN_DIR='$COMPANY_DIR/domains'/\$DOMAIN
+
+        SUBS=\"\$DOMAIN_DIR/subs.txt\"
+
+        BRUTE_DNS=\"\$DOMAIN_DIR/brute_dns.txt\"
+
+        mkdir -p \"\$DOMAIN_DIR\"
+
+        echo '[+] Iniciando recon para' \$DOMAIN
+
+        subfinder -d \$DOMAIN -silent | anew \"\$SUBS\"
+
+        if $ENABLE_BRUTE; then
+
+            echo '[+] Executando shuffleDNS para' \$DOMAIN
+
+            shuffledns \
+                -d \$DOMAIN \
+                -w /usr/share/wordlists/SecLists/Discovery/DNS/dns-Jhaddix.txt \
+                -r /usr/share/wordlists/resolvers/resolvers.txt \
+                -o \"\$BRUTE_DNS\" \
+                -t 1000 \
+                -mode bruteforce
+
+            cat \"\$BRUTE_DNS\" | anew \"\$SUBS\"
+
+        fi
+
+    " :::: "$domain_path"
+
+done
---
+++ vuln_recon.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export PATH=$PATH:$HOME/go/bin
+BUG_BOUNTY_DIR="/home/kali/bugbounty"
+
+find "$BUG_BOUNTY_DIR" -type f -name "subs.txt" | parallel  -j 4 --bar '
+		SUBS_FILE={}
+		DOMAIN_DIR=$(dirname "$SUBS_FILE")
+		DOMAIN=$(basename "$DOMAIN_DIR")
+		NUCLEI_OUT="$DOMAIN_DIR/nuclei_out.txt"
+		
+		echo "[+] Iniciando Vuln Recon para $DOMAIN"
+		
+		nuclei -l "$SUBS_FILE" -severity high,critical | anew "$NUCLEI_OUT" | notify -id vulns
+	'
